47f1afaeb0ac65da46e20bacfea38faf466c3cbb
devklick <devklick@gmail.com>
2022-06-20 18:49:53 +0100

    Trying to fix package.json import

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 import { program } from "commander";
+import path from "path";
 import cli from "./src/lib/cli";
 
-cli(program, require("./package.json").version);
+cli(program, require(path.join(__dirname, "package.json")).version);
